611a64ba48135f13ff2c0ea56232f066c6363dc5
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test>
2023-06-12 07:33:13 -0300

    Projeto Viagem

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { DetalharComponent } from './detalhar/detalhar.component';
+import { ExcluirComponent } from './excluir/excluir.component';
+import { ListarComponent } from './listar/listar.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'listar', component:ListarComponent},
+  {path:'listar/excluir', component:ExcluirComponent},
+  {path:'listar/detalhar', component:DetalharComponent},
+  {path:'listar/cadastrar', component:CadastrarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListarComponent } from './listar/listar.component';
+import { ExcluirComponent } from './excluir/excluir.component';
+import { DetalharComponent } from './detalhar/detalhar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarComponent,
+    ExcluirComponent,
+    DetalharComponent,
+    CadastrarComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
